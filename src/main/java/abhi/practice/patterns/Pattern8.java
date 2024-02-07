b0f1f8e551f30e22c38feba0a018af1433f74b07
Abhinav Prakash <email@abhinav-prakash.de>
2024-02-07 16:48:29 +0100

    Pattern8

---
+++ src/main/java/abhi/practice/patterns/Pattern8.java
@@ -0,0 +1,33 @@
+    package abhi.practice.patterns;
+
+/**
+ *
+ *    * * * * * * * * *
+ *      * * * * * * *
+ *        * * * * *
+ *          * * *
+ *            *
+ */
+public class Pattern8 {
+    public static void main(String[] args) {
+        System.out.println("First pattern is:");
+        createPattern(4);
+        System.out.println("\n");
+        System.out.println("Second pattern is:");
+        createPattern(5);
+    }
+
+    private static void createPattern(int numOfRows) {
+        int lastRowIndex = numOfRows - 1;
+        for (int row = 0; row < numOfRows; row++) {
+            for (int column = 0; column <= (2*lastRowIndex + 1); column++) {
+                if((column >= row) && (column <= (2*lastRowIndex -row))) {
+                    System.out.print("*  ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
