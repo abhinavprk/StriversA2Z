c93a65d5953cceded94030550739055af596acc4
Abhinav Prakash <email@abhinav-prakash.de>
2024-02-08 13:59:37 +0100

    Pattern9 and Pattern10

--- src/main/java/abhi/practice/patterns/Pattern9.java
+++ src/main/java/abhi/practice/patterns/Pattern9.java
@@ -23,12 +23,24 @@ public class Pattern9 {
 
     private static void createPattern(int numOfRows) {
         int lastRowIndex = numOfRows - 1;
+        //Print upper half
+        for (int row = 0; row < numOfRows; row++) {
+            for (int column = 0; column <= (2*lastRowIndex + 1); column++) {
+                if((column >= lastRowIndex - (row)) && (column <= (lastRowIndex + row))) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        //Print lower half
         for (int row = 0; row < numOfRows; row++) {
             for (int column = 0; column <= (2*lastRowIndex + 1); column++) {
                 if((column >= row) && (column <= (2*lastRowIndex -row))) {
-                    System.out.print("*  ");
+                    System.out.print("* ");
                 } else {
-                    System.out.print("   ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
