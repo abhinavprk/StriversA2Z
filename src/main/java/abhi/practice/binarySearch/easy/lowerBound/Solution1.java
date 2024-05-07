f880b47694db575b5ec0b1a2dc6471fd9b735d3b
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-07 20:17:01 +0530

    solution

---
+++ src/main/java/abhi/practice/binarySearch/easy/lowerBound/Solution1.java
@@ -0,0 +1,48 @@
+package abhi.practice.binarySearch.easy.lowerBound;
+
+/**
+ * Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+ *
+ * Example 1:
+ * Input Format:
+ *  N = 4, arr[] = {1,2,2,3}, x = 2
+ * Result:
+ *  1
+ * Explanation:
+ *  Index 1 is the smallest index such that arr[1] >= x.
+ *
+ * Example 2:
+ * Input Format:
+ *  N = 5, arr[] = {3,5,8,15,19}, x = 9
+ * Result:
+ *  3
+ * Explanation:
+ *  Index 3 is the smallest index such that arr[3] >= x.
+ *
+ */
+public class Solution1 {
+
+    public static void main(String[] args) {
+        System.out.println(findLowerBound(new int[]{1,2,2,3}, 2));
+        System.out.println(findLowerBound(new int[]{3,5,8,15,19}, 5));
+    }
+
+    private static int findLowerBound(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int result = high;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        if((nums[high]>=target)){
+            result = high;
+        }
+
+        return result;
+    }
+}
